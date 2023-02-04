2fb0df22219f3161aee67ebe6edd4d44cefa366f
pvianap <pedro.v.parente@gmail.com>
2023-02-04 12:17:37 +0100

    config router

---
+++ src/Router/index.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import EmployeeForm from '../pages/EmployeeForm';
+import EmployeeList from '../pages/EmployeeList';
+import NotFound from '../pages/NotFound';
+
+export default function Routing() {
+  return (
+    <Routes>
+      <Route path="/" element={<Outlet />}>
+        <Route index element={<EmployeeForm />} />
+        <Route path="list" element={<EmployeeList />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+}
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      <p>404 page not found</p>
+    </div>
+  );
+}
