ac4a033f04e2bb0cb190c2f6b76f2df9566bb462
pvianap <pedro.v.parente@gmail.com>
2023-03-19 20:52:40 +0100

    changing library for tables

--- src/pages/EmployeeList/index.jsx
+++ src/pages/EmployeeList/index.jsx
@@ -1,38 +1,87 @@
 import React from 'react';
-import DataTable from 'react-data-table-component';
+// import DataTable from 'react-data-table-component';
 import { useSelector } from 'react-redux';
+import { useTable } from 'react-table';
 
 export default function EmployeeList() {
   const employees = useSelector((state) => state.employees.employeesList);
 
-  const columns = [
-    {
-      name: 'First Name',
-      selector: 'firstName',
-      sortable: true,
-    },
-    {
-      name: 'Last Name',
-      selector: 'lastName',
-      sortable: true,
-    },
-    {
-      name: 'Department',
-      selector: 'department',
-      sortable: true,
-    },
-    {
-      name: 'Date of Birth',
-      selector: 'dateOfBirth',
-      sortable: true,
-    },
-    {
-      name: 'Address',
-      selector: (row) =>
-        `${row.street}, ${row.city}  ${row.stateAbbr} ${row.zipCode}`,
-      sortable: true,
-    },
-  ];
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'First Name',
+        accessor: 'firstName', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Last Name',
+        accessor: 'lastName',
+      },
+      {
+        Header: 'Department',
+        accessor: 'department',
+      },
+      {
+        Header: 'Date of Birth',
+        accessor: 'dateOfBirth',
+      },
+      {
+        Header: 'Address',
+        accessor: (row) =>
+          `${row.street}, ${row.city}  ${row.stateAbbr} ${row.zipCode}`,
+      },
+    ],
+    []
+  );
 
-  return <DataTable title="Employee List" columns={columns} data={employees} />;
+  const tableInstance = useTable({ columns, data });
+
+  const {
+    getTableProps,
+    getTableBody,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  // const columns = [
+  //   {
+  //     name: 'First Name',
+  //     selector: 'firstName',
+  //     sortable: true,
+  //   },
+  //   {
+  //     name: 'Last Name',
+  //     selector: 'lastName',
+  //     sortable: true,
+  //   },
+  //   {
+  //     name: 'Department',
+  //     selector: 'department',
+  //     sortable: true,
+  //   },
+  //   {
+  //     name: 'Date of Birth',
+  //     selector: 'dateOfBirth',
+  //     sortable: true,
+  //   },
+  //   {
+  //     name: 'Address',
+  //     selector: (row) =>
+  //       `${row.street}, ${row.city}  ${row.stateAbbr} ${row.zipCode}`,
+  //     sortable: true,
+  //   },
+  // ];
+
+  return (
+    <Table {...getTableProps()}>
+      <TableHead>
+        {headerGroups.map((headerGroup) => (
+          <TableRow {...headerGroup.getHeaderGroupProps}>
+            {headerGroup.map}
+          </TableRow>
+        ))}
+      </TableHead>
+    </Table>
+  );
+  // <DataTable title="Employee List" columns={columns} data={employees} />;
 }
