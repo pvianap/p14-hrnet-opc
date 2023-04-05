9ffcff5dc0b0dff36ae4ed16c3c3a4ca37440b7a
pvianap <pedro.v.parente@gmail.com>
2023-04-05 13:06:40 -0300

    pagination

--- src/pages/EmployeeList/index.jsx
+++ src/pages/EmployeeList/index.jsx
@@ -1,10 +1,129 @@
 import React from 'react';
 // import DataTable from 'react-data-table-component';
 import { useSelector } from 'react-redux';
-import { useTable } from 'react-table';
+import { useTable, useSortBy, usePagination} from 'react-table';
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    rows,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state:{pageIndex, pageSize},
+  } = useTable({
+    columns,
+    data,
+    initialState:{pageIndex:0},
+  }, 
+  useSortBy, usePagination)
+
+  // Render the UI for your table
+  return (
+    <>
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map(
+            (row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    )
+                  })}
+                </tr>
+              )}
+          )}
+        </tbody>
+    </table>
+    {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  )
+}
 
 export default function EmployeeList() {
-  const employees = useSelector((state) => state.employees.employeesList);
+  const data = useSelector((state) => state.employees.employeesList);
 
   const columns = React.useMemo(
     () => [
@@ -43,45 +162,7 @@ export default function EmployeeList() {
     prepareRow,
   } = tableInstance;
 
-  // const columns = [
-  //   {
-  //     name: 'First Name',
-  //     selector: 'firstName',
-  //     sortable: true,
-  //   },
-  //   {
-  //     name: 'Last Name',
-  //     selector: 'lastName',
-  //     sortable: true,
-  //   },
-  //   {
-  //     name: 'Department',
-  //     selector: 'department',
-  //     sortable: true,
-  //   },
-  //   {
-  //     name: 'Date of Birth',
-  //     selector: 'dateOfBirth',
-  //     sortable: true,
-  //   },
-  //   {
-  //     name: 'Address',
-  //     selector: (row) =>
-  //       `${row.street}, ${row.city}  ${row.stateAbbr} ${row.zipCode}`,
-  //     sortable: true,
-  //   },
-  // ];
-
   return (
-    <Table {...getTableProps()}>
-      <TableHead>
-        {headerGroups.map((headerGroup) => (
-          <TableRow {...headerGroup.getHeaderGroupProps}>
-            {headerGroup.map}
-          </TableRow>
-        ))}
-      </TableHead>
-    </Table>
-  );
-  // <DataTable title="Employee List" columns={columns} data={employees} />;
-}
+      <Table columns={columns} data={data} />
+  )
+  }
